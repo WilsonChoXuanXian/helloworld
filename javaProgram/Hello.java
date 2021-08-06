0776a7f08ee33c036e9609d43f3aadc781e1198f
Cho Xuan Xian <1201302646@student.mmu.edu.my>
2021-08-07 02:59:55 +0800

    Integer.parseInt(x)

--- javaProgram/Hello.java
+++ javaProgram/Hello.java
@@ -2,7 +2,6 @@ package javaProgram;
 
 public class Hello {
     public static void main(String[] args) {
-        
         String x = "Hello World!!!";
         System.out.println(x);
     }
--- javaProgram/Main.java
+++ javaProgram/Main.java
@@ -1,8 +1,16 @@
 package javaProgram;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String x = "10";
+        short s = Short.parseShort(x);      // String to short
+        System.out.println(s);
+
+        int y = Integer.parseInt(x);        // String to integer
+        System.out.println(y);
+
+        x = "10.5";
+        double z = Double.parseDouble(x);   // String to double
+        System.out.println(z);
     }
 }
